90d77ccc46890978a4c0c55b1139cd1a5f5b7086
Daniel Sim <sqds2@cam.ac.uk>
2014-10-11 09:00:54 +0200

    pretty!

---
+++ migros.py
@@ -0,0 +1,14 @@
+
+def SelectRandomUser():
+	pass
+
+def GenerateQuestions(user):
+	return range(1,16)
+
+
+def SavePreferences(user, question_list, answers):
+	pass
+
+def PersonalityTest(question_list, answers):
+	pass
+
--- quizapp.py
+++ quizapp.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# coding=utf-8
+import migros
 import flask
 from flask import Flask
 from flask import render_template, session, request
@@ -17,10 +20,27 @@ def question(step):
 	Ask the user the question for this step
 	"""
 
-	if 'question_list' not in flask.session:
-		flask.session['question_list'] = [] ## initialize this to a list of categories
+	if int(request.args['begin']):
+		session.clear()
+
+	user = None
+
+	if 'question_list' not in flask.session or \
+			'answers' not in flask.session:
+		session['question_list'] = migros.GenerateQuestions(user) ## initialize this to a list of categories
+		session['answers'] = [ None ] * len(session['question_list'])
 	else:
 		print 'Resued question list %s' % str(flask.session['question_list'])
+
+	if 'question_num' in request.args:
+		qn_num = int(request.args['question_num'])
+		if qn_num >= 1 and qn_num <= len(session['question_list']):
+			if request.args['clicked'] == 'left':
+				session['answers'][qn_num] = 0
+			elif request.args['clicked'] == 'right':
+				session['answers'][qn_num] = 1
+			else:
+				session['answers'][qn_num] = None
 
 	total_questions = len(flask.session['question_list'])
 	args = {
@@ -45,7 +65,45 @@ def results():
 	"""
 	Give the user the results of the quiz
 	"""
-	pass
+
+	user = None
+	migros.SavePreferences( user, session['question_list'], session['answers'] )
+	migros.PersonalityTest( session['question_list'], session['answers'] )
+
+	args = {
+		'enumerate': enumerate,
+		'personality': [
+			{
+			'adjective': 'miserly',
+			'description': """You would never spend on non-Migros Budget
+				products if you had a choice.
+				""",
+			'image': '/static/images/poorkids.png',
+			},
+
+			{
+			'adjective': 'proudly Swiss!',
+			'description': u"""You kauftest immer die Schweizer Produkte,
+			auch wenn die Ausländische Produkte günstiger sind.
+				""",
+			'image': '/static/images/flag_switzerland.svg',
+			},
+
+			{
+			'adjective': 'Environmentally Indifferent!',
+			'description': """
+				You couldn't care less if all the ice in Greenland
+				and Antarctica melted tomorrow.
+				Or you don't believe in climate change.
+				Or both.
+				""",
+			'image': '/static/images/Igloo_landscape.svg',
+			},
+
+			]
+		}
+
+	return render_template('results.html', **args)
 
 if __name__ == '__main__':
 	app.run()
