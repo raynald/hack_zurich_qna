2d23bbedd7f59af49f3a14f7424ab80ef9466f5b
Raynald Chung <raynaldchung@gmail.com>
2014-10-11 17:19:59 +0200

    ;ksdfj;alksdjf;as

--- migros.py
+++ migros.py
@@ -2,6 +2,9 @@ import json
 import query as qy
 import random
 import numpy as np
+import pymongo as mg
+
+coll = mg.MongoClient()['migros']['lebensmittel']
 
 def RandNumGen(x, y):
     return random.randint(x, y-1)
@@ -25,50 +28,133 @@ def GenerateQuestions(user):
         num = coll.find( {
             'lowest_category': subtit,
             'organic': True,
+            'info.image.medium': {'$exists':True},
         }).count()
         if len(bios) < 5 and coll.find( {
             'lowest_category': subtit,
             'organic': {'$exists': True},
+            'info.image.medium': {'$exists':True},
         }).count() > num and num > 0:
             bios.append(subtit)
+            print 'added'
             continue
 
         num = coll.find( {
             'lowest_category': subtit,
             'country': 'CH',
+            'info.image.medium': {'$exists':True},
         }).count()
         if len(schweizs) < 5 and coll.find( {
             'lowest_category': subtit,
             'country': {'$exists': True},
+            'info.image.medium': {'$exists':True},
         }).count() > num and num > 0:
             schweizs.append(subtit)
             continue
-        
+
         num = coll.find( {
             'lowest_category': subtit,
             'environment': {'$gte': 0.5},
+            'info.image.medium': {'$exists':True},
         }).count()
         if len(umwelt) < 5 and coll.find( {
             'lowest_category': subtit,
             'environment': {'$exists': True},
+            'info.image.medium': {'$exists':True},
         }).count() > num and num > 0:
             umwelt.append(subtit)
             continue
-        
+
         num = coll.find( {
             'lowest_category': subtit,
             'health': {'$gte': 7},
+            'info.image.medium': {'$exists':True},
         }).count()
         if len(gesundheit) < 5 and coll.find( {
             'lowest_category': subtit,
             'health': {'$exists': True},
+            'info.image.medium': {'$exists':True},
         }).count() > num and num > 0:
             gesundheit.append(subtit)
             continue
-        
+
         if len(gesundheit) >= 5 and len(umwelt) >= 5  \
             and len(schweizs) >= 5 and len(bios) >= 5:
             break
+
+    Pdts = []
+
+    for c in bios:
+        Pdts.append( (
+          coll.find_one( {
+            'lowest_category': c,
+            'organic': True,
+            'info.image.medium': {'$exists':True},
+          }),
+          coll.find_one( {
+            'lowest_category': c,
+            'organic': False,
+            'info.image.medium': {'$exists':True},
+          }),
+          ))
+
+    for c in schweizs:
+        Pdts.append( (
+          coll.find_one( {
+            'lowest_category': c,
+            'country': 'CH',
+            'info.image.medium': {'$exists':True},
+          }),
+          coll.find_one( {
+            'lowest_category': c,
+            'country': {'$ne': 'CH'},
+            'info.image.medium': {'$exists':True},
+          }),
+          ))
+
+    for c in umwelt:
+        Pdts.append( (
+          coll.find_one( {
+            'lowest_category': c,
+            'environment': {'$gte': 0.5},
+            'info.image.medium': {'$exists':True},
+          }),
+          coll.find_one( {
+            'lowest_category': c,
+            'environment': {'$lt': 0.5},
+            'info.image.medium': {'$exists':True},
+          }),
+          ))
+
+    for c in gesundheit:
+        Pdts.append( (
+          coll.find_one( {
+            'lowest_category': c,
+            'health': {'$gte': 7},
+            'info.image.medium': {'$exists':True},
+          }),
+          coll.find_one( {
+            'lowest_category': c,
+            'health': {'$lt': 7},
+            'info.image.medium': {'$exists':True},
+          }),
+          ))
+
+        return [({'health':x['health'],
+            'organic':x['organic'],
+            'country':x['country'],
+            'environment':x['environment'],
+            'name':x['name'],
+            'id':x['id'],
+            'image':x['info']['image']['medium'] if 'image' in x['info'] else None
+            }, {'health':y['health'],
+            'organic':y['organic'],
+            'country':y['country'],
+            'environment':y['environment'],
+            'name':y['name'],
+            'id':y['id'],
+            'image':y['info']['image']['medium'] if 'image' in y['info'] else None
+            }) for (x,y) in Pdts]
 
 def SavePreferences(user, question_list, answers):
 	pass
--- quizapp.py
+++ quizapp.py
@@ -9,101 +9,123 @@ app = Flask(__name__)
 app.debug = True
 app.secret_key='this key is very secret indeed'
 
+@app.route('/')
 @app.route('/quiz')
 def quiz():
-	# Render the quiz start page
-	return render_template('quiz.html')
+    # Render the quiz start page
+    return render_template('quiz.html')
 
 @app.route('/question/<step>')
 def question(step):
-	"""
-	Ask the user the question for this step
-	"""
+    """
+    Ask the user the question for this step
+    """
 
-	if int(request.args['begin']):
-		session.clear()
+    if 'begin' in request.args and int(request.args['begin']):
+        session.clear()
 
-	user = None
+    step = int(step)
+    print 'Step is %d' % step
 
-	if 'question_list' not in flask.session or \
-			'answers' not in flask.session:
-		session['question_list'] = migros.GenerateQuestions(user) ## initialize this to a list of categories
-		session['answers'] = [ None ] * len(session['question_list'])
-	else:
-		print 'Resued question list %s' % str(flask.session['question_list'])
+    user = None
 
-	if 'question_num' in request.args:
-		qn_num = int(request.args['question_num'])
-		if qn_num >= 1 and qn_num <= len(session['question_list']):
-			if request.args['clicked'] == 'left':
-				session['answers'][qn_num] = 0
-			elif request.args['clicked'] == 'right':
-				session['answers'][qn_num] = 1
-			else:
-				session['answers'][qn_num] = None
+    if 'question_list' not in flask.session or \
+            'answers' not in flask.session:
+        session['question_list'] = migros.GenerateQuestions(user) ## initialize this to a list of categories
+        session['answers'] = [ None ] * len(session['question_list'])
+    else:
+        print 'Resued question list %s' % str(flask.session['question_list'])
 
-	total_questions = len(flask.session['question_list'])
-	args = {
-			'image_left_source': 'http://migros-cache.fsi-viewer.com/fsicache/server?type=image&source=images%2Fmigros_api%2Fstaging%2Fproduct_204015800000.jpg',
-			'image_right_source': 'http://migros-cache.fsi-viewer.com/fsicache/server?type=image&source=images%2Fmigros_api%2Fstaging%2Fproduct_204015800000.jpg',
-			'statkeys': ['Fat', 'Sodium', 'Energy'],
-			'leftstat': {'Fat': '100g', 'Sodium': '1mg', 'Energy': '1kcal'},
-			'rightstat': {'Fat': '100g', 'Sodium': '1mg', 'Energy': '1kcal'},
-			'question_num': step,
-			'total_questions': 15,
-			'form_action': '/question/%d' % (int(step)+1),
-			};
+    if 'question_num' in request.args:
+        qn_num = int(request.args['question_num'])
+        if qn_num >= 1 and qn_num <= len(session['question_list']):
+            if request.args['clicked'] == 'left':
+                session['answers'][qn_num - 1] = -1
+            elif request.args['clicked'] == 'right':
+                session['answers'][qn_num - 1] = 1
+            else:
+                session['answers'][qn_num - 1] = 0
 
-	if step > total_questions:
-		args['form_action'] = '/results'
+    total_questions = len(flask.session['question_list'])
+    country_flag_map = {
+            'ES': '/static/images/spain.png',
+            'CH': '/static/images/switzerland.svg',
+            'DE': '/static/images/germany.gif',
+            'AT': '/static/images/austria.gif',
+            'IT': '/static/images/italy.gif',
+            'FR': '/static/images/france.gif',
+    }
 
-	# Render the quiz start page
-	return render_template('question.html', step=step, **args)
+    if step >= total_questions:
+        return results()
+
+    args = {
+            'image_left_source': session['question_list'][step][0]['image'],
+            #'http://migros-cache.fsi-viewer.com/fsicache/server?type=image&source=images%2Fmigros_api%2Fstaging%2Fproduct_204015800000.jpg',
+            'image_right_source': session['question_list'][step][1]['image'],
+            #'http://migros-cache.fsi-viewer.com/fsicache/server?type=image&source=images%2Fmigros_api%2Fstaging%2Fproduct_204015800000.jpg',
+            'lefthealth': session['question_list'][step][0]['health'],
+            'righthealth': session['question_list'][step][1]['health'],
+            'leftcountry': country_flag_map[session['question_list'][step][0]['country']],
+            'rightcountry': country_flag_map[session['question_list'][step][1]['country']],
+            'leftorganic': session['question_list'][step][0]['organic'],
+            'rightorganic': session['question_list'][step][1]['organic'],
+            'leftenvironment': session['question_list'][step][0]['environment'],
+            'rightenvironment': session['question_list'][step][1]['environment'],
+            'question_num': step,
+            'total_questions': 15,
+            'form_action': '/question/%d' % (int(step)+1),
+            'leftname': session['question_list'][step][0]['name'],
+            'rightname': session['question_list'][step][1]['name'],
+            };
+
+    # Render the quiz start page
+    return render_template('question.html', step=step, **args)
 
 @app.route('/results')
 def results():
-	"""
-	Give the user the results of the quiz
-	"""
+    """
+    Give the user the results of the quiz
+    """
 
-	user = None
-	migros.SavePreferences( user, session['question_list'], session['answers'] )
-	migros.PersonalityTest( session['question_list'], session['answers'] )
+    user = None
+    migros.SavePreferences( user, session['question_list'], session['answers'] )
+    migros.PersonalityTest( session['question_list'], session['answers'] )
 
-	args = {
-		'enumerate': enumerate,
-		'personality': [
-			{
-			'adjective': 'miserly',
-			'description': """You would never spend on non-Migros Budget
-				products if you had a choice.
-				""",
-			'image': '/static/images/poorkids.png',
-			},
+    args = {
+        'enumerate': enumerate,
+        'personality': [
+            {
+            'adjective': 'miserly',
+            'description': """You would never spend on non-Migros Budget
+                products if you had a choice.
+                """,
+            'image': '/static/images/poorkids.png',
+            },
 
-			{
-			'adjective': 'proudly Swiss!',
-			'description': u"""You kauftest immer die Schweizer Produkte,
-			auch wenn die Ausländische Produkte günstiger sind.
-				""",
-			'image': '/static/images/flag_switzerland.svg',
-			},
+            {
+            'adjective': 'proudly Swiss!',
+            'description': u"""You kauftest immer die Schweizer Produkte,
+            auch wenn die Ausländische Produkte günstiger sind.
+                """,
+            'image': '/static/images/flag_switzerland.svg',
+            },
 
-			{
-			'adjective': 'Environmentally Indifferent!',
-			'description': """
-				You couldn't care less if all the ice in Greenland
-				and Antarctica melted tomorrow.
-				Or you don't believe in climate change.
-				Or both.
-				""",
-			'image': '/static/images/Igloo_landscape.svg',
-			},
+            {
+            'adjective': 'Environmentally Indifferent!',
+            'description': """
+                You couldn't care less if all the ice in Greenland
+                and Antarctica melted tomorrow.
+                Or you don't believe in climate change.
+                Or both.
+                """,
+            'image': '/static/images/Igloo_landscape.svg',
+            },
 
-			]
-		}
+            ]
+        }
 
-	return render_template('results.html', **args)
+    return render_template('results.html', **args)
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
